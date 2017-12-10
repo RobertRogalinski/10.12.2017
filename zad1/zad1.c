f5da74351421f402adf41b82a760ad8b5821f410
Robert Rogaliński <rrogalinski@linux-p110-15.inf.ug.edu.pl>
2017-12-10 13:03:48 +0100

    zad1

---
+++ zad1/zad1.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+void sumatablic(int tab1[3][3], int tab2[3][3], int tab3[3][3])
+{
+int a,b;
+for(a=0; a<3; a++)
+{ for(b=0; b<3; b++)
+tab3[a][b]=tab1[a][b]+tab2[a][b];
+}
+}
+
+int main()
+{
+  int a,b;
+  int tablica1[3][3];
+  int tablica2[3][3];
+  int tablica3[3][3];
+  printf("Podaj tablice 1: \n");
+  for(a=0; a<3; a++)
+
+  { for(b=0; b<3; b++)
+
+  scanf("%d",&tablica1[a][b]);
+  }
+
+  { for(b=0; b<3; b++)
+
+  scanf("%d",&tablica1[a][b]);
+  }
+  printf("Podaj tablice 2: \n");
+  for(a=0; a<3; a++)
+
+  { for(b=0; b<3; b++)
+
+  scanf("%d",&tablica2[a][b]);
+  }
+printf("PODALES: \n");
+
+  for(a=0; a<3; a++)
+
+  { for(b=0; b<3; b++)
+
+  printf("%d ",tablica1[a][b]);
+  }
+  putchar('\n');
+  for(a=0; a<3; a++)
+
+  { for(b=0; b<3; b++)
+
+  printf("%d ",tablica2[a][b]);
+  }
+    putchar('\n');
+  sumatablic(tablica1,tablica2,tablica3);
+  printf("Po uzyciu funkcji: \n");
+  for(a=0; a<3; a++)
+
+  { for(b=0; b<3; b++)
+
+  printf("%d ",tablica3[a][b]);
+}
+return 0;
+}
